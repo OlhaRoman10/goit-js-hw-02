19e2a829bb1b67bc8437dd92acd355d5db56024c
Olha_Roman <ovgrozdova79@gmail.com>
2025-11-09 17:37:40 +0100

    uppercase

--- js/task-6.js
+++ js/task-6.js
@@ -1,4 +1,14 @@
 function getSubstring(string, length) {
     return string.slice(0, length)
 }
+
+function normalizeInput(input, to) {
+  if (to === "upper") {
+    return input.toUpperCase();
+  } else {
+    return input.toLowerCase();
+  }
+}
+
 console.log(getSubstring("Hello world", 3));
+console.log(normalizeInput("das IST SpAm", "upper"));
