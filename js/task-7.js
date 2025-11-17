07a1a4450a5e116594ebcb7528919fae86aeee54
Olha_Roman <ovgrozdova79@gmail.com>
2025-11-17 17:17:12 +0100

    break2

---
+++ js/task-7.js
@@ -0,0 +1,16 @@
+function findNumber(max, target) {
+	console.log("Log in the body of the function before the cycle");
+
+  for (let i = 5; i <= max; i += 1) {
+    console.log("Current counter value i:", i);
+
+    if (i === target) {
+      console.log(`Found the number $ {target}, interrupt the cycle`);
+			break;
+    }
+  }
+
+  console.log("Log in body function after cycle");
+}
+
+    console.log(findNumber(20, 16));
